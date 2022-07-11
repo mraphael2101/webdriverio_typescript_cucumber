bd0153481be3a91dc190cb6552985e818ee1285c
mraphael2101 <uk.mark.raphael@gmail.com>
2022-07-11 14:14:19 +0100

    Working WDIO config

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,23 +1,21 @@
 import {Options} from "@wdio/types";
 
-export const config: Options.Testrunner = {
+//export const config: Options.Testrunner = {
+export const config: WebdriverIO.Config = {
     capabilities: [{
         browserName: 'chrome',
         maxInstances: 1,
     }],
     cucumberOpts: {
-        //require: ['./test/**/*.ts'],
-        //strict: false,
-        //dryRun: false,
+        require: ['./test/**/*.ts'],
+        strict: false,
+        dryRun: false,
     },
     specs: [
         './test/**/*.feature'
     ],
-    framework: 'jasmine',
-    jasmineOpts: {
-        defaultTimeoutInterval: 5000,
-        expectationResultHandler: function (passed, assertion) {},
-    },
+    services: ['chromedriver'],
+    framework: 'cucumber',
     autoCompileOpts: {
         autoCompile: true,
         tsNodeOpts: {
